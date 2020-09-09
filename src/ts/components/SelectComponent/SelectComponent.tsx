de483b25f7611e9d8e02e129e4c44b924e1a1060
Alexey Radziuk <radyukag@yandex.ru>
2020-09-09 09:50:58 +0300

    feat: add key property to option elements

--- src/ts/components/SelectComponent/SelectComponent.tsx
+++ src/ts/components/SelectComponent/SelectComponent.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { Select } from 'antd';
 
 const SelectComponent: React.FunctionComponent<{ values: string[], handler: (value: string) => void }> = ({ values, handler }) => {
-    const options = values.map(type => <Select.Option value={type}>{type}</Select.Option>);
+    const options = values.map(type => <Select.Option value={type} key={type}>{type}</Select.Option>);
     const [value, setValue] = useState(values[0]);
     const changeHandler: (value: string) => void = (value) => {
         setValue(value);
