155b1c513f665e0023fdff997e40dacbdf6de9bd
Evgeniya <evgeniya375447249020@gmail.com>
2020-09-13 14:58:26 +0300

    feat: add task table

---
+++ src/ts/components/TaskTable/TaskTable.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+type TaskTableType = {
+  deadline: string;
+  title: string;
+};
+
+const TaskTable: React.FC<TaskTableType> = ({ deadline, title }) => {
+  return (
+    <table className="table-task">
+      <tbody>
+        <tr>
+          <th>Deadline</th>
+          <th>Folder name</th>
+          <th>Branch name</th>
+        </tr>
+        <tr>
+          <td>{deadline}</td>
+          <td>{title.toLowerCase()}</td>
+          <td>{title.toLowerCase()}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default TaskTable;
