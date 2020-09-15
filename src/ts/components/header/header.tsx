b98b243e9d84eab5ee20a5f5d57cd50c78130277
Evgeniya <evgeniya375447249020@gmail.com>
2020-09-15 13:55:26 +0300

    feat: add logo rschool'

--- src/ts/components/header/header.tsx
+++ src/ts/components/header/header.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Layout } from 'antd';
 import SelectComponent from '../SelectComponent/SelectComponent';
-import typesOfScheduleForm from './typesOfScheduleForm';
 import typesOfUsers from './typesOfUsers';
-import Title from '../Title/title';
+import SaveFile from '../SaveFile/SaveFile';
 
 const Header: React.FunctionComponent<{
   selectUserHandler: (value: string) => void;
-  selectScheduleFormHandler: (value: string) => void;
-}> = ({ selectUserHandler, selectScheduleFormHandler }) => {
+}> = ({ selectUserHandler }) => {
   return (
     <Layout.Header className="header">
-      <Title />
-      <div>
-        <SelectComponent values={typesOfUsers} handler={selectUserHandler} />
-        <SelectComponent values={typesOfScheduleForm} handler={selectScheduleFormHandler} />
+      <div className="header__logo">
+        <img src="https://app.rs.school/static/images/logo-rsschool3.png" alt="rs school logo" />
       </div>
+      <h2 className="header__title">Shedule</h2>
+      <SelectComponent values={typesOfUsers} handler={selectUserHandler} />
     </Layout.Header>
   );
 };
