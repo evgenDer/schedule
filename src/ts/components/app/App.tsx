188c9af490e46583794e9d89da1ece4efe0864d1
Evgeniya <evgeniya375447249020@gmail.com>
2020-09-15 13:57:55 +0300

    feat: add controls

--- src/ts/components/app/App.tsx
+++ src/ts/components/app/App.tsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import Header from '../header/header';
+import { SheduleType } from '../SelectTypeShedule/SheduleType';
+import SelectTypeShedule from '../SelectTypeShedule/SelectTypeShedule';
+import SaveFile from '../SaveFile/SaveFile';
+import { Button, Tooltip } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
 
 const App: React.FC = () => {
   const [typeOfUser, setTypeOfUser] = useState('student');
-  const [typeOfScheduleForm, setTypeOfScheduleForm] = useState('table');
+  const [typeOfScheduleForm, setTypeOfScheduleForm] = useState(SheduleType.Table);
   const typesOfUsersHandler: (value: string) => void = function (value) {
     setTypeOfUser(value);
   };
-  const typesOfScheduleFormHandler: (value: string) => void = (value) => {
-    setTypeOfScheduleForm(value);
-  };
   return (
     <React.Fragment>
-      <Header selectUserHandler={typesOfUsersHandler} selectScheduleFormHandler={typesOfScheduleFormHandler} />
+      <Header selectUserHandler={typesOfUsersHandler} />
+      <div className="controls">
+        <SelectTypeShedule type={typeOfScheduleForm} setType={setTypeOfScheduleForm} />
+        <SaveFile />
+        <Button>
+          <EditOutlined /> Edit types
+        </Button>
+      </div>
     </React.Fragment>
   );
 };
