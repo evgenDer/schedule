2132636aa3c8ae767f42c8ef22bcd0a7ab04701f
Simba1987 <simbad.fly@gmail.com>
2020-09-16 11:49:31 +0300

    feat: add task type color assembly

--- src/ts/components/EditingSchedule/EditingSchedule.tsx
+++ src/ts/components/EditingSchedule/EditingSchedule.tsx
@@ -1,26 +1,49 @@
 import React, { useState } from 'react';
 import { Modal, Button, Menu, Tag } from 'antd';
 import { EditOutlined, PlusCircleOutlined } from '@ant-design/icons';
-import { TASK_TYPES, TASK_TYPES_COLOR } from '../../constants/taskTypes';
+import { TASK_TYPES, TASK_TYPES_BACKGROUND_COLOR, TASK_TYPES_FONT_COLOR } from '../../constants/taskTypes';
 import { makingListTags, makingListColors } from './makeListItems';
-
 
 function EditingSchedule() {
   const[visible, setVisible] = useState(false);
   const { SubMenu } = Menu;
-  let selectedTaskType: string = '';
-  // let actualColorSelectedType: string = '';
+  let customizationTypeTask: any = {};
+  let taskType: string = '';
 
   const handleOkBtn = (): void => {
     setVisible(false);
+    localStorage.setItem('typeTask', JSON.stringify(customizationTypeTask));
     // console.log('work', TASK_TYPES);
+    console.log(customizationTypeTask);
   }
 
   const handleSelectTaskType = (item: any): void => {
-    selectedTaskType = item.item.node.innerText;
-    // const copyTaskType: any = {...TASK_TYPES};
-    // actualColorSelectedType = copyTaskType[item.key]['color'];
-    console.log('select item: ', selectedTaskType, item);
+    const copyTaskType: any = {...TASK_TYPES};
+    taskType = item.key;
+    customizationTypeTask = {[taskType]: Object};
+    customizationTypeTask[taskType] = copyTaskType[taskType];
+    // console.log('select item: ', item, taskType, customizationTypeTask);
+  }
+
+  const handleSelectColorTaskType = (item: any): void => {
+    const colorTaskType = item.item.node.innerText;
+    customizationTypeTask[taskType]['color'] = colorTaskType;
+    // console.log('select color for task type: ', colorTaskType, customizationTypeTask);
+  }
+
+  const handleSelectBackgroundTaskType = (item: any): void => {
+    const fontColorTaskType = item.item.node.innerText;
+    customizationTypeTask[taskType]['fontColor'] = fontColorTaskType;
+  }
+
+  const handleSelectBackgroundColorDescription = (item: any): void => {
+    const fontColorTaskType = item.item.node.innerText;
+    customizationTypeTask[taskType]['descriptionBackgroundColor'] = fontColorTaskType;
+  }
+
+  const handleSelectFontColorDescription = (item: any): void => {
+    const fontColorTaskType = item.item.node.innerText;
+    customizationTypeTask[taskType]['descriptionBackgroundColor'] = fontColorTaskType;
   }
 
   return (
@@ -51,11 +74,45 @@ function EditingSchedule() {
         <Menu
           mode={'vertical'}
           theme={'light'}
-          // defaultSelectedKeys={[actualColorSelectedType]} // Актуальный цвет выбранного типа задания
+          onSelect={(item) => handleSelectColorTaskType(item)}
         >
-          <SubMenu key="sub2" icon={<PlusCircleOutlined />} title="Select color for selected type">
-            <Menu.ItemGroup title='Select type'>
-              {makingListColors(TASK_TYPES_COLOR)}
+          <SubMenu key="sub2" icon={<PlusCircleOutlined />} title="Background color for selected type(optional)">
+            <Menu.ItemGroup title='Background color'>
+              {makingListColors(TASK_TYPES_BACKGROUND_COLOR)}
+            </Menu.ItemGroup>
+          </SubMenu>
+        </Menu>
+        <Menu
+          mode={'vertical'}
+          theme={'light'}
+          onSelect={(item) => handleSelectBackgroundTaskType(item)}
+        >
+          <SubMenu key="sub2" icon={<PlusCircleOutlined />} title="Font color for selected type(optional)">
+            <Menu.ItemGroup title='Font color'>
+              {makingListColors(TASK_TYPES_FONT_COLOR)}
+            </Menu.ItemGroup>
+          </SubMenu>
+        </Menu>
+        <hr />
+        <Menu
+          mode={'vertical'}
+          theme={'light'}
+          onSelect={(item) => handleSelectBackgroundColorDescription(item)}
+        >
+          <SubMenu key="sub2" icon={<PlusCircleOutlined />} title="Background color for description(optional)">
+            <Menu.ItemGroup title='Background color'>
+              {makingListColors(TASK_TYPES_FONT_COLOR)}
+            </Menu.ItemGroup>
+          </SubMenu>
+        </Menu>
+        <Menu
+          mode={'vertical'}
+          theme={'light'}
+          onSelect={(item) => handleSelectFontColorDescription(item)}
+        >
+          <SubMenu key="sub2" icon={<PlusCircleOutlined />} title="Font color for description(optional)">
+            <Menu.ItemGroup title='Font color'>
+              {makingListColors(TASK_TYPES_FONT_COLOR)}
             </Menu.ItemGroup>
           </SubMenu>
         </Menu>
--- src/ts/constants/taskTypes.ts
+++ src/ts/constants/taskTypes.ts
@@ -1,19 +1,41 @@
 import { ITaskType } from './types-interfaces';
 
 export const TASK_TYPES = {
-  jstask: { name: 'js task', color: 'green' } as ITaskType,
-  deadline: { name: 'deadline', color: 'red' } as ITaskType,
-  test: { name: 'test', color: 'volcano' } as ITaskType,
-  interview: { name: 'interview', color: 'purple' } as ITaskType,
-  codewars: { name: 'codewars', color: 'lime' } as ITaskType,
-  cvmarkdown: { name: 'cv:markdown', color: 'geekblue' } as ITaskType,
-  cvhtml: { name: 'cv:html', color: 'geekblue' } as ITaskType,
-  htmlcssacademy: { name: 'htmlcssacademy', color: 'blue' } as ITaskType,
-  lecture: { name: 'lecture', color: 'magenta' } as ITaskType,
+  jstask: { 
+    name: 'js task', color: 'green', fontColor: 'green', descriptionBackgroundColor: '#fff', descriptionFontColor: '#000' 
+  } as ITaskType,
+  deadline: { 
+    name: 'deadline', color: 'red', fontColor: 'red', descriptionBackgroundColor: '#fff', descriptionFontColor: '#000' 
+  } as ITaskType,
+  test: { 
+    name: 'test', color: 'volcano', fontColor: 'volcano', descriptionBackgroundColor: '#fff', descriptionFontColor: '#000'
+  } as ITaskType,
+  interview: { 
+    name: 'interview', color: 'purple', fontColor: 'purple', descriptionBackgroundColor: '#fff', descriptionFontColor: '#000'
+  } as ITaskType,
+  codewars: { 
+    name: 'codewars', color: 'lime', fontColor: 'lime', descriptionBackgroundColor: '#fff', descriptionFontColor: '#000'
+  } as ITaskType,
+  cvmarkdown: { 
+    name: 'cv:markdown', color: 'geekblue', fontColor: 'geekblue', descriptionBackgroundColor: '#fff', descriptionFontColor: '#000'
+  } as ITaskType,
+  cvhtml: { 
+    name: 'cv:html', color: 'geekblue', fontColor: 'geekblue', descriptionBackgroundColor: '#fff', descriptionFontColor: '#000'
+  } as ITaskType,
+  htmlcssacademy: { 
+    name: 'htmlcssacademy', color: 'blue', fontColor: 'blue', descriptionBackgroundColor: '#fff', descriptionFontColor: '#000'
+ } as ITaskType,
+  lecture: { 
+    name: 'lecture', color: 'magenta', fontColor: 'magenta', descriptionBackgroundColor: '#fff', descriptionFontColor: '#000'
+  } as ITaskType,
 };
 
-export const TASK_TYPES_COLOR = [
+export const TASK_TYPES_BACKGROUND_COLOR = [
   'magenta', 'red', 'volcano', 'orange', 'gold', 'lime', 'green', 'cyan', 'blue', 'geekblue', 'purple'
 ];
+
+export const TASK_TYPES_FONT_COLOR = [
+  '#000000', '#f00000', '#fff700', '#d9d9d9', '#1bff00', '#00ffdc', '#0037ff', '#d800ff'
+]
 
 // export default TASK_TYPES;
