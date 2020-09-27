acb26b87130143cb09109da28f971109c1da86a7
Vladislav Zheromsky <vladislavzheromsky@gmail.com>
2020-09-27 11:51:29 +0300

    fix: columns storaging

--- src/ts/helpers/storage.ts
+++ src/ts/helpers/storage.ts
@@ -23,7 +23,7 @@ export const setSelectedColumns = (selectedColumns: string[]) => {
 };
 
 export const getSelectedColumns = (): string[] => {
-  return JSON.parse(localStorage.getItem('RSSSelectedColumns') || '[]');
+  return JSON.parse(localStorage.getItem('RSSSelectedColumns') || '["name", "taskDone"]');
 };
 
 export const setHiddenRows = (hiddenRows: string[]) => {
