8ad366431079427cf18f26bb4a0c109a8093edd6
Evgeniya <evgeniya375447249020@gmail.com>
2020-09-27 17:36:45 +0300

    feat: add save file in new format

---
+++ src/ts/helpers/downaload.ts
@@ -0,0 +1,8 @@
+export const download = (filename, text) => {
+  const element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  element.click();
+}
